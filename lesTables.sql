02b8311c01296f3f784a60d6b99451c7910c07f5
Artur01300 <artur.karapetyan@outlook.fr>
2021-02-08 11:58:20 +0100

    Modification

--- lesTables.sql
+++ lesTables.sql
@@ -18,14 +18,14 @@ USE `groupomania` ;
 -- Table `groupomania`.`users`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `groupomania`.`users` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_user` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(30) NOT NULL,
   `email` VARCHAR(40) NOT NULL,
   `password` VARCHAR(50) NOT NULL,
   `isAdmin` TINYINT(1) NULL,
   `idimage` VARCHAR(255) NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `idusers_UNIQUE` (`id` ASC) VISIBLE)
+  PRIMARY KEY (`id_user`),
+  UNIQUE INDEX `idusers_UNIQUE` (`id_user` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
@@ -33,18 +33,18 @@ ENGINE = InnoDB;
 -- Table `groupomania`.`articles`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `groupomania`.`articles` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_article` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(200) NOT NULL,
   `content` TEXT CHARACTER SET 'ascii' NOT NULL,
   `create_at` DATETIME NOT NULL,
   `imarge` VARCHAR(255) NULL,
   `users_id_user` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  PRIMARY KEY (`id_article`),
+  UNIQUE INDEX `id_UNIQUE` (`id_article` ASC) VISIBLE,
   INDEX `fk_articles_users_idx` (`users_id_user` ASC) VISIBLE,
   CONSTRAINT `fk_articles_users`
     FOREIGN KEY (`users_id_user`)
-    REFERENCES `groupomania`.`users` (`id`)
+    REFERENCES `groupomania`.`users` (`id_user`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -54,23 +54,23 @@ ENGINE = InnoDB;
 -- Table `groupomania`.`commentaires`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `groupomania`.`commentaires` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_comment` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `content` TEXT NOT NULL,
   `created_at` DATETIME NOT NULL,
   `articles_id_article` INT UNSIGNED NOT NULL,
   `users_id_user` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  PRIMARY KEY (`id_comment`),
+  UNIQUE INDEX `id_UNIQUE` (`id_comment` ASC) VISIBLE,
   INDEX `fk_commentaires_articles1_idx` (`articles_id_article` ASC) VISIBLE,
   INDEX `fk_commentaires_users1_idx` (`users_id_user` ASC) VISIBLE,
   CONSTRAINT `fk_commentaires_articles1`
     FOREIGN KEY (`articles_id_article`)
-    REFERENCES `groupomania`.`articles` (`id`)
+    REFERENCES `groupomania`.`articles` (`id_article`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_commentaires_users1`
     FOREIGN KEY (`users_id_user`)
-    REFERENCES `groupomania`.`users` (`id`)
+    REFERENCES `groupomania`.`users` (`id_user`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -80,29 +80,29 @@ ENGINE = InnoDB;
 -- Table `groupomania`.`likes_dislikes`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `groupomania`.`likes_dislikes` (
-  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_likedislike` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `like_dislike` TINYINT(1) NULL,
   `commentaires_id_comment` INT UNSIGNED NOT NULL,
   `articles_id_article` INT UNSIGNED NOT NULL,
   `users_id_user` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id`, `articles_id_article`, `users_id_user`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
+  PRIMARY KEY (`id_likedislike`, `articles_id_article`, `users_id_user`),
+  UNIQUE INDEX `id_UNIQUE` (`id_likedislike` ASC) VISIBLE,
   INDEX `fk_likes_dislikes_commentaires1_idx` (`commentaires_id_comment` ASC) VISIBLE,
   INDEX `fk_likes_dislikes_articles1_idx` (`articles_id_article` ASC) VISIBLE,
   INDEX `fk_likes_dislikes_users1_idx` (`users_id_user` ASC) VISIBLE,
   CONSTRAINT `fk_likes_dislikes_commentaires1`
     FOREIGN KEY (`commentaires_id_comment`)
-    REFERENCES `groupomania`.`commentaires` (`id`)
+    REFERENCES `groupomania`.`commentaires` (`id_comment`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_likes_dislikes_articles1`
     FOREIGN KEY (`articles_id_article`)
-    REFERENCES `groupomania`.`articles` (`id`)
+    REFERENCES `groupomania`.`articles` (`id_article`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_likes_dislikes_users1`
     FOREIGN KEY (`users_id_user`)
-    REFERENCES `groupomania`.`users` (`id`)
+    REFERENCES `groupomania`.`users` (`id_user`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
